05d7aecb08c159c57805bb48335d6943e0377cd7
diwas-dahal <diwasdahal08@gmail.com>
2021-10-27 21:33:44 +0545

    Final Commit

--- RES.py
+++ RES.py
@@ -1,6 +1,23 @@
 import random
 import time
-import cryptomath
+
+
+def gcd(a, b):
+    while a != 0:
+        a, b = b % a, a
+    return b
+
+
+def findModInverse(a, m):
+    if gcd(a, m) != 1:
+        return None
+    u1, u2, u3 = 1, 0, a
+    v1, v2, v3 = 0, 1, m
+    while v3 != 0:
+        q = u3 // v3
+        v1, v2, v3, u1, u2, u3 = (
+            u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
+    return u1 % m
 
 
 def isPrime(num):
@@ -41,10 +58,10 @@ def GenerateKey(KeySize=1024):
     e = None
     while True:
         e = random.randrange(2 ** (KeySize - 1), 2 ** KeySize)
-        if cryptomath.gcd(e, (p - 1) ** (q - 1)) == 1:
+        if gcd(e, (p - 1) ** (q - 1)) == 1:
             break
 
-    d = cryptomath.findModInverse(e, (p - 1) * (q - 1))
+    d = findModInverse(e, (p - 1) * (q - 1))
 
     publicKey = (n, e)
     privateKey = (n, d)
